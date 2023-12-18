be4c5ff4aa0aada31130ad53a8a5ed4b7bc92a91
AdityaRajaram <aditya.rajaram13@gmail.com>
2023-12-18 16:25:09 +0530

    changes

--- src/components/impressum/ImpressumBackdrop.jsx
+++ src/components/impressum/ImpressumBackdrop.jsx
@@ -4,8 +4,7 @@ const ImpressumBackdrop = () => {
             <div className="bg-opacity-0  fixed  top-0 left-0 right-0  bg-center bg-cover "></div>
             <div className="  flex-col absolute inset-0 flex items-center justify-center bg-opacity-50 text-white text-center">
                 <h2 className="text-5xl  font-bold relative">
-                    So funktioniert‘s
-                </h2>
+                Impressum     </h2>
                 
             </div>
             <br></br>
