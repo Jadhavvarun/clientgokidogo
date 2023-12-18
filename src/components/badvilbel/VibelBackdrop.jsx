26c6e95cc354d49f12dacf2960e6456460146024
AdityaRajaram <aditya.rajaram13@gmail.com>
2023-12-18 17:29:04 +0530

    changes

--- src/components/badvilbel/VibelBackdrop.jsx
+++ src/components/badvilbel/VibelBackdrop.jsx
@@ -12,7 +12,7 @@ export default function VibelBackdrop({ filteredData }) {
 
   return (
     <div className="h-[400px] w-auto relative">
-      <div className="bg-opacity-0 fixed top-0 left-0 right-0 bg-center bg-cover "></div>
+      <div className="bg-opacity-0 fixed top-0 left-0 right-0  "></div>
       <div className="flex-col absolute inset-0 flex items-center justify-center bg-opacity-50 text-white text-center">
         <h2 className="text-5xl font-bold relative">
           {records} 
